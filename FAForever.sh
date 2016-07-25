c1b99766bed31276aa697c29d484b67bc8575cea
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-07-26 01:26:07 +0200

    Updated FAF lobby download, use newer version of wine, fix bug after downloading a new wine version

--- FAForever.sh
+++ FAForever.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env playonlinux-bash
 # Date : (2016-07-09 01-08)
-# Last revision : (2016-07-09 20-44)
-# Wine version used : 1.9.13
+# Last revision : (2016-07-26 01-07)
+# Wine version used : 1.9.13, 1.9.15
 # Distribution used to test : Linux Mint 17.2 Rafaela
 # Author : Walkman
 [ "$PLAYONLINUX" = "" ] && exit 0
@@ -19,16 +19,17 @@ POL_SetupWindow_presentation "$TITLE" "github.com/orgs/FAForever/people" "fafore
 POL_System_TmpCreate "$PREFIX"
     cd "$POL_System_TmpDir"
     POL_Download "https://download.microsoft.com/download/5/B/C/5BC5DBB3-652D-4DCE-B14A-475AB85EEF6E/vcredist_x86.exe" "b88228d5fef4b6dc019d69d4471f23ec"
-    POL_Download "http://content.faforever.com/Forged.Alliance.Forever-0.11.58-win32.msi" "52c9dc652264ef9c97614c518658acfc"
+    POL_Download "http://content.faforever.com/Forged.Alliance.Forever-0.11.60-win32.msi" "ae3e27708989aa2bc6e6b2d18cefe7ad"
 
     POL_Wine_SelectPrefix "$PREFIX"
-    POL_Wine_PrefixCreate "1.9.13"
+    POL_Wine_PrefixCreate "1.9.15"
 
+    cd "$POL_System_TmpDir"
     POL_SetupWindow_wait "Installing Microsoft Visual C++ 2010 Redistributable Package (x86)..." "$TITLE"
     POL_Wine "vcredist_x86.exe"
 
     POL_SetupWindow_wait "Installing FAF lobby..." "$TITLE"
-    POL_Wine msiexec /i "Forged.Alliance.Forever-0.11.58-win32.msi" /q
+    POL_Wine msiexec /i "Forged.Alliance.Forever-0.11.60-win32.msi" /q
 
     POL_Call POL_Install_d3dx9 # required for game to run
 POL_System_TmpDelete
